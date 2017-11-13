6abb950e67289297cabd30d293c9f747b0edb5ce
Aleksandr <alexar120996@gmail.com>
2017-11-13 22:01:45 +0200

    maybe update

--- calculator.hs
+++ calculator.hs
@@ -1,26 +1,22 @@
 type Operator = Double -> Double -> Double
-type newOperator = (String, Operator)
-type Register = [newOperator]
-
+type NewOperator = (String, Operator)
+type Register = [NewOperator]
 operators :: Register
 operators = [
                 ("+", (+)),
                 ("-", (-)),
                 ("*", (*)),
                 ("/", (/))
-            ]
+            ]            
             
-main = print $ calculate "2 + 2 * 2"
-            
-calculate :: String -> Double
-calculate = parseStep operators . words
-            
-parseStep :: Register -> [String] -> Double
+calculate :: String -> Maybe  Double
+calculate = parseStep operators . words          
+parseStep :: Register -> [String] -> Maybe Double
 parseStep _ [number] = read number
 parseStep ((operator, function):xs) unparsed =
     case span (/=operator) unparsed of
         (_, []) -> parseStep xs unparsed
         (beforeOperator, afterOperator) -> 
             function
-                (parseStep operators beforeOperator)
-                (parseStep operators $ drop 1 afterOperator)
+                <$> (parseStep operators beforeOperator)
+                <*> (parseStep operators $ drop 1 afterOperator)
